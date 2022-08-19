0b0b8140e43af98e61542bbe3c43109f1b5f5b9a
Miguel <migueleduagui@gmail.com>
2022-08-19 12:23:41 -0300

    definimos endpoints

--- routes/characters.router.mjs
+++ routes/characters.router.mjs
@@ -15,4 +15,79 @@ router.get('/', async(req,res,next) =>{
     }
 } );
 
+//endpoint para Creacion de characters
+router.post('/', async(req,res,next) =>{
+    try {
+        const body = req.body;
+        //const image = req.files.image;
+
+        res.status(201).json({
+            message:'created',
+            data:{
+                ...body,
+            }
+        })
+    } catch (error) {
+        //next(error);
+    }
+});
+
+//endpoint para actualizacion de characters.
+router.put('/:id', async(req,res,next) =>{
+    try {
+        const {id} = req.params;
+        const body = req.body;
+        //const image = req.files.image;
+
+        res.status(201).json({
+            message:"update",
+            data:body,
+            id
+        })
+    } catch (error) {
+        //next(error);
+    }
+})
+
+//endpoint para actualizacion parcial de characters.
+router.patch('/:id', async(req,res,next) =>{
+    try {
+        const {id} = req.params;
+        const body = req.body;
+        //const image = req.files.image;
+
+        res.status(201).json({
+            message:'update',
+            data: body,
+            id,
+        });
+    } catch (error) {
+        //next(error);
+    }
+})
+
+//endpoint para eliminacion de characters
+router.delete('/:id', async(req,res,next) =>{
+    try {
+        const {id} = req.params;
+
+        res.status(201).json({
+            message:'deleted',
+            id,
+        });
+    } catch (error) {
+        //next(error);
+    }
+});
+
+/* Characters
+
+Image
+Name
+Age
+Heigth
+History
+Movies
+s
+*/
 export default router;
