adbb8b0050e3f07db63c64b3d59dc0eb2a3c7898
Miguel <migueleduagui@gmail.com>
2022-08-19 10:11:08 -0300

    definimos endpoints

--- routes/categories.router.mjs
+++ routes/categories.router.mjs
@@ -1,5 +1,6 @@
 import express from "express";
 
+
 //Declaramos una variable router que contendrá el metodo router de express.
 const router = express.Router();
 
@@ -14,6 +15,66 @@ router.get('/', async(req,res,next) =>{
         //next(error);
     }
 } );
+
+//endpoint para Creacion de categorias
+router.post('/', async(req,res,next) =>{
+    try {
+        const body = req.body;
+
+        res.status(201).json({
+            message:'created',
+            data:body
+        })
+    } catch (error) {
+        //next(error);
+    }
+});
+
+//endpoint para actualizacion de categorias.
+router.put('/:id', async(req,res,next) =>{
+    try {
+        const {id} = req.params;
+        const body = req.body;
+
+        res.status(201).json({
+            message:"update",
+            data:body,
+            id
+        })
+    } catch (error) {
+        //next(error);
+    }
+})
+
+//endpoint para actualizacion parcial de categorias.
+router.patch('/:id', async(req,res,next) =>{
+    try {
+        const {id} = req.params;
+        const body = req.body;
+
+        res.status(201).json({
+            message:'update',
+            data: body,
+            id,
+        });
+    } catch (error) {
+        //next(error);
+    }
+})
+
+//endpoint para eliminacion de categorias
+router.delete(':/id', async(req,res,next) =>{
+    try {
+        const {id} = req.params;
+
+        res.status(201).json({
+            message:'deleted',
+            id,
+        });
+    } catch (error) {
+        //next(error);
+    }
+});
 
 //Accion/aventura
 //Animacion
